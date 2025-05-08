8731aea9af3f1401cd5700bb58a83d2041f52363
N'guettia Cedric <iko.drico@gmail.com>
2025-05-08 12:31:36 +0000

    page secondaire

---
+++ components/sections/CtaEnergie.js
@@ -0,0 +1,24 @@
+
+import Link from "next/link"
+
+export default function CtaEnergie() {
+    return (
+        <>
+            <section className="call-to-action">
+                <div className="bg bg-image" style={{ backgroundImage: 'url(./images/background/bg4.jpg)' }}></div>
+                <div className="auto-container">
+                    <div className="outer-box wow fadeIn">
+                        <h2 className="title"> 
+                         Activer intelligemment les processus et le contenu pour accélérer le flux des activités
+                            {/* <br /> */}
+                            <br />
+                            Contactez notre équipe commerciale !
+                        </h2>
+                        <Link href="/page-contact" className="theme-btn btn-style-one light"><span className="btn-title-text-white">Contactez-nous</span></Link>
+                    </div>
+                </div>
+                
+            </section>
+        </>
+    )
+}
--- components/sections/EnergiesRenouvelable.js
+++ components/sections/EnergiesRenouvelable.js
@@ -1,42 +1,275 @@
 
 import Link from "next/link"
+import CtaEnergie from "./CtaEnergie"
 
 export default function EnergiesRenouvelable() {
     return (
         <>
 
-            <section className="services-section-two pt-5">
-                <div className="bg bg--12" />
-                <div className="auto-container">
-                    <div className="sec-title text-center black">
-                        {/* <span className="sub-title">Découvrez</span> */}
-                        <h2>Énergies renouvelables <br/></h2>
+            {/*  Details  */}
+                <section className="team-details">
+                    <div className="container pb-0">
+                      <div className="team-details__top pb-0">
+                        <div className="row">
+                          <div className="col-xl-6 col-lg-6 col-md-6 mt-5">
+                            <div className="team-details__top-left">
+                              <div className="team-details__top-img">
+                                {" "}
+                                <img
+                                  src="images/resource/solaire-1--min.jpg"
+                                  alt="solution divalto"
+                                />
+                              </div>
+                            </div>
+                          </div>
+                          <div
+                            className="col-xl-6 col-lg-6 col-md-6"
+                            style={{ marginTop: "42px" }}
+                          >
+                            <div className="team-details__top-right">
+                              <div className="team-details__top-content">
+                                <h3 className=""> ENERGIE SOLAIRE</h3>
+                                {/* <p className="" style={{ textAlign: "justify" }}>
+                                  Divalto est un éditeur français de logiciels de gestion
+                                  d’entreprise (ERP et CRM) fondé en 1982. Sa mission est
+                                  d’accompagner les PME et ETI dans leur transformation
+                                  digitale, en leur proposant des solutions modulaires,
+                                  puissantes et évolutives. <br />
+                                  Deux grandes familles de solutions sont proposées :
+                                </p> */}
+                                <h6 className="title text-black">
+                                  <i className="icon fa fa-circle-check" />  Etude, fourniture et installation de sites solaires (centrales solaires photovoltaïques(jusqu’à plusieurs Mégawatts), de chauffes eaux solaires et de pompages solaires).{" "}
+                                </h6>
+            
+                                <h5 className="title text-black">
+                                  <i className="icon fa fa-circle-check" />  Réhabilitation de sites solaires (électricité solaire et pompage solaire).{" "}
+                                </h5>
+                                <h5 className="title text-black">
+                                  <i className="icon fa fa-circle-check" /> SAV {" "}
+                                </h5>
+                                <h5 className="title text-black">
+                                  <i className="icon fa fa-circle-check" /> Leasing solaire(financement de votre installation solaire).{" "} 
+                                </h5>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
                     </div>
+                </section>
+            
+                {/* */}
+                <section className="team-details mb-0">
+                <div className="container pb-0">
+                    <div className="team-details__top pb-0">
                     <div className="row">
-                        {/* Service Block Two */}
-                        <div className="news-block service-block-two col-lg-4 col-md-6 coll-md-12 wow fadeInUp">
-                            <div className="inner-box">
-                                <div className="image-box">
-                                    <figure className="image"><img src="/images/resource/b1.png" alt="solaire" /></figure>
-                                </div>
-                                <div className="title-box">
-                                    <h5 className="title"><Link href="/page-service-details">Études et installation de centrales solaires</Link></h5>
-                                </div>
-                                <div className="content-box">
-                                    <i className="icon flaticon-digital-services" />
-                                    <div className="text">Lorem Ipsum is simply dummy text of the printing and typesetting industry...</div>
+                        <div className="col-xl-6 col-lg-6 col-md-6 mt-0">
+                        <div className="team-details__top-left">
+                            <div className="team-details__top-content">
+                            <h3 className="">
+                                Audit énergétique pour entreprises et collectivités
+                            </h3>
+                            <p className="" style={{ textAlign: "justify" }}>
+                                L’audit énergétique consiste à réaliser un diagnostic de la performance énergétique de l’entreprise, puis de planifier des actions d’amélioration. ISO 50 001 vise trois grands aspects :
+                            </p>   
+                            <div className="info-box mt-3">
+                                <div className="inner">
+                                <h5 className="title">
+                                    <i className="icon fa fa-circle-check" /> Réduire sa facture énergétique {" "}
+                                </h5>
                                 </div>
                             </div>
+                            <div className="info-box mt-3">
+                                <div className="inner">
+                                <h5 className="title">
+                                    <i className="icon fa fa-circle-check" />{" "} Améliorer la performance énergétique de l’organisation{" "}
+                                </h5>
+                                </div>
+                            </div>
+                            <div className="info-box mt-3">
+                                <div className="inner">
+                                <h5 className="title">
+                                    <i className="icon fa fa-circle-check" /> Diminuer son impact carbone{" "}
+                                </h5>
+                                </div>
+                            </div>
+                            </div>
                         </div>
-                        {/* Service Block Two */}
-                       
+                        </div>
+                        <div className="col-xl-6 col-lg-6 col-md-6">
+                        <div className="team-details__top-right">
+                            <div className="team-details__top-img">
+                            {" "}
+                            <img src="images/resource/divalto-3.png" alt="Divalto" />
+                            </div>
+                        </div>
+                        </div>
                     </div>
-
-                  
-
-                    
+                    </div>
                 </div>
-            </section>
+                </section>
+                {/* */}
+
+
+                {/*  */}
+                <section className="team-details">
+                    <div className="container pb-0">
+                      <div className="team-details__top pb-0">
+                        <div className="row">
+                          <div className="col-xl-6 col-lg-6 col-md-6 mt-5">
+                            <div className="team-details__top-left">
+                              <div className="team-details__top-img">
+                                {" "}
+                                <img
+                                  src="images/resource/solaire-1--min.jpg"
+                                  alt="solution divalto"
+                                />
+                              </div>                    
+
+                            </div>
+                          </div>
+                          <div
+                            className="col-xl-6 col-lg-6 col-md-6"
+                            style={{ marginTop: "42px" }}
+                          >
+                            <div className="team-details__top-right">
+                              <div className="team-details__top-content">
+                                <h3 className=""> Suivi et reporting des émissions carbon</h3>
+                                <p className="" style={{ textAlign: "justify" }}>
+                                Les services d'efficacité énergétique permettent notamment de mieux maîtriser sa consommation, donc son budget énergétique. Ils constituent un élément de réponse important à la croissance de la demande énergétique et à la réduction des émissions de CO2.
+
+                                </p>
+                                <h6 className="title text-black">
+                                  <i className="icon fa fa-circle-check" /> Intégration de smart grids et systèmes de monitoring. {" "}
+                                </h6>
+            
+                                <h6 className="title text-black">
+                                  <i className="icon fa fa-circle-check" /> Solutions hybrides (solaire + stockage) pour zones isolées.{" "}
+                                </h6>    
+                                
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                </section>
+                {/*  */}
+                {/* <div className="container pb-0">
+                <div className="team-details__top pb-0">
+                    <div className="row">
+                    <h3 className="title text-center">
+                        Découvrez les logiciels ERP <br /> métiers de Divalto
+                    </h3>
+        
+                    <div className="col-xl-4 col-lg-4 col-md-4 mt-5">
+                        <div className="team-details__top-left">
+                        <div className="team-details__top-img">
+                            {" "}
+                            <img src="images/resource/divalto-1.png" alt="" />
+                            <div className="info-box mt-3">
+                            <div className="inner">
+                                <h5 className="title text-center">
+                                Divalto business
+                                </h5>
+                                <p>
+                                Contrairement à certains ERP plus complexes, Odoo est
+                                connu pour sa prise en main rapide, sans besoin de
+                                formations intensives :
+                                </p>
+                                <h5 className="title">
+                                <i className="icon fa fa-circle-check" /> {""}{" "}
+                                l’ensemble des canaux de vente,{""}
+                                </h5>
+                                <h5 className="title">
+                                <i className="icon fa fa-circle-check" /> {""} la chaine
+                                logistique,{""}
+                                </h5>
+                                <h5 className="title">
+                                <i className="icon fa fa-circle-check" /> {""} la
+                                complexité des tarifs,{""}
+                                </h5>
+                                <h5 className="title">
+                                <i className="icon fa fa-circle-check" /> {""} les
+                                catalogues fournisseurs,{""}
+                                </h5>
+                                <h5 className="title">
+                                <i className="icon fa fa-circle-check" /> {""} les
+                                dernières innovations attendus par les acteurs du
+                                secteur.{""}
+                                </h5>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+        
+                    <div className="col-xl-4 col-lg-4 col-md-4 mt-5">
+                        <div className="team-details__top-left">
+                        <div className="team-details__top-img">
+                            {" "}
+                            <img src="images/resource/divalto-7.png" alt="" />
+                            <div className="info-box mt-3">
+                            <div className="inner">
+                                <h5 className="title text-center">
+                                Divalto field service
+                                </h5>
+                                <p>
+                                Les entreprises offrant des services terrain ont besoin
+                                d’être toujours plus réactives et de bénéficier d’une
+                                visibilité en temps réel sur toutes les interactions de
+                                l’entreprise (clients, fournisseurs, …). Notre Logiciel
+                                ERP-CRM tout terrain, Divalto field service :
+                                </p>
+                                <h5 className="title">
+                                <i className="icon fa fa-circle-check" /> gère
+                                d’importantes flottes multi-agences de techniciens,{""}
+                                </h5>
+                                <h5 className="title">
+                                <i className="icon fa fa-circle-check" /> {""} suit la
+                                rentabilité des affaires et contrats pour accompagner la
+                                croissance.,{""}
+                                </h5>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    <div className="col-xl-4 col-lg-4 col-md-4 mt-5">
+                        <div className="team-details__top-left">
+                        <div className="team-details__top-img">
+                            {" "}
+                            <img src="images/resource/divalto-6.png" alt="" />
+                            <div className="info-box mt-3">
+                            <div className="inner">
+                                <h5 className="title text-center">
+                                Divalto industry
+                                </h5>
+                                <p>
+                                L’industrie est en pleine transformation : développement
+                                et fabrication de produits de plus en plus
+                                personnalisés, renforcement du service client, …
+                                Nombreux industriels sont à la recherche de solutions
+                                leur permettant de mieux gérer leurs coûts et de
+                                valoriser leur savoir-faire. Notre logiciel ERP-CRM gère
+                                toutes les catégories de données et offre, entre-autre,
+                                une ouverture vers l’IoT qu’elles adoptent largement.
+                                </p>
+                                
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                </div> */}
+                {/*  */}
+            
+
+                <CtaEnergie />
+            
+                 
 
             
         </>
